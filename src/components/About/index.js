5e9b66900c5eaa66c3b8a2fd8b6e96a8eeaef67f
Michael Hoffmann <kontakt@mokkapps.de>
2018-09-03 19:33:51 +0200

    Added timeline to about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,14 +3,19 @@ import React from 'react';
 import './About.scss';
 
 import SkillBar from '../SkillBar';
+import Timeline from '../Timeline';
 import AboutMe from './AboutMe';
 
-const About = ({aboutImage}) => (
+const About = ({ aboutImage }) => (
   <section>
     <AboutMe aboutImage={aboutImage} />
     <div className="about__skills">
       <h1 className="about__skills-heading">Skills</h1>
       <SkillBar />
+    </div>
+    <div className="about__skills">
+      <h1 className="about__skills-heading">Timeline</h1>
+      <Timeline />
     </div>
   </section>
 );
