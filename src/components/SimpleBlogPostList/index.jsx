946f80b1faad772b1e8829651a0ae77f4dd7f4a6
Michael Hoffmann <michael.hoffmann@mokkapps.de>
2021-05-26 10:00:12 +0200

    improved minimal blog post list

--- src/components/SimpleBlogPostList/index.jsx
+++ src/components/SimpleBlogPostList/index.jsx
@@ -1,34 +1,65 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-`;
+import { FormattedMessage } from 'react-intl';
 
 const SimpleBlogPostList = props => {
   const { items } = props;
 
+  const yearPostMap = {};
+
+  for (const item of items) {
+    const {
+      fields: { prefix },
+    } = item;
+
+    const year = prefix.substring(0, 4);
+
+    if (!yearPostMap[year]) {
+      yearPostMap[year] = [item];
+    } else {
+      yearPostMap[year].push(item);
+    }
+  }
+
+  const sortedYears = Object.keys(yearPostMap).sort(function (a, b) {
+    return b.localeCompare(a);
+  });
+
   return (
-    <Container>
-      <ul>
-        {items.map(item => {
-          const {
-            frontmatter: { title },
-            fields: { slug, prefix },
-          } = item;
-          return (
-            <li key={slug}>
-              <Link to={`/blog${slug}`}>
-                {prefix}: {title}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </Container>
+    <section className="flex flex-col justify-center">
+      {sortedYears.map((year, index) => {
+        const posts = yearPostMap[year];
+        return (
+          <div key={year + index} className="flex flex-col my-4">
+            <div className="flex items-center justify-between">
+              <h2 className="">{year}</h2>
+              <p className="italic mb-4">
+                <FormattedMessage
+                  id="minimalBlogListPage.publishedPosts"
+                  values={{ amount: posts.length }}
+                />
+              </p>
+            </div>
+            <ul>
+              {posts.map(post => {
+                const {
+                  frontmatter: { title },
+                  fields: { slug, prefix },
+                } = post;
+                return (
+                  <li key={slug}>
+                    <Link to={`/blog${slug}`}>
+                      {prefix}: {title}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        );
+      })}
+    </section>
   );
 };
 
