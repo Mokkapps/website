bf9ac78d885e2996fd4807e8bffaf27a62420521
Michael Hoffmann <michael.hoffmann@mokkapps.de>
2022-01-16 14:18:10 +0100

    [skip netlify]
    Added basic RSS feed test

---
+++ cypress/integration/rss_spec.js
@@ -0,0 +1,21 @@
+describe('RSS Test', () => {
+  let urls = [];
+
+  it('should succesfully load each url in the feed', () => {
+    cy.request('/rss.xml')
+      .as('sitemap')
+      .then(response => {
+        urls = Cypress.$(response.body)
+          .find('link')
+          .toArray()
+          .map(el => el.nextSibling.data)
+          .filter((_, index) => index !== 0)
+          .map(url => url.replace('https://www.mokkapps.de', ''));
+        console.log(urls);
+      })
+      .and(() => {
+        expect(urls).to.have.length(5);
+        urls.forEach(cy.visit);
+      });
+  });
+});
