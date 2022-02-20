573d76c9d0dd352945e0c83ea73e1fe3df645207
Michael Hoffmann <michael.hoffmann@mokkapps.de>
2022-02-20 19:45:06 +0100

    [skip netlify]
    renamed tips menu to Vue tips

--- src/content/meta/menu.js
+++ src/content/meta/menu.js
@@ -3,7 +3,7 @@ const menu = [
   { i18nId: 'menu.about', to: '/about', ariaLabel: 'About' },
   { i18nId: 'menu.contact', to: '/contact', ariaLabel: 'Contact' },
   { i18nId: 'menu.blog', to: '/blog', ariaLabel: 'Blog' },
-  { i18nId: 'menu.tips', to: '/tips', ariaLabel: 'Tips' },
+  { i18nId: 'menu.tips', to: '/tips', ariaLabel: 'Vue Tips' },
   { i18nId: 'menu.newsletter', to: '/newsletter', ariaLabel: 'Newsletter' },
   { i18nId: 'menu.stats', to: '/stats', ariaLabel: 'stats' },
   { i18nId: 'menu.projects', to: '/projects', ariaLabel: 'Projects' },
